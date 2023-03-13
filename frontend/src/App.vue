ffb3f23149bddc4b6ad1aaded8c17fadf9d23d12
barathbalasubramanian <barathkumar.b2411@gmail.com>
2023-03-13 17:44:53 +0530

    transparent-header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,85 +1,88 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+	import { RouterLink, RouterView } from 'vue-router'
+	import HelloWorld from './components/HelloWorld.vue'
+	export default {
+	data() {
+		return {
+			scrolled: false
+		};
+	},
+	created() {
+    	this.handleScroll();
+  	},
+	methods: {
+		handleScroll() {
+			setInterval(() => {
+				this.scrolled = window.pageYOffset >= 10; }, 200);
+			}
+		}
+	};
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+	<div>
+		
+		<header class="container-fluid fixed-top bg-white d-flex justify-content-center align-items-center header" :class="{ scrolled : scrolled  }">
+			<div>Serve Arena</div>
+		</header>
+		<div class="pages">
+			<div class="page p1 vh-100 vw-100"></div>
+			<div class="page p2 vh-100 vw-100"></div>
+			<div class="page p3 vh-100 vw-100"></div>
+		</div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+	</div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+
+.text {
+  text-shadow: 3px 3px 10px #666;
+  text-decoration: none;
+  border-left: 10px dotted #E8CD26;
+  border-bottom: 10px dotted #666;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.header {
+	padding: 24px !important;
+    -webkit-backdrop-filter: blur(60px);
+    border-radius: 0 0 32px 32px;
+	transition: background-color 0.3s ease-in-out;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.scrolled {
+	background-color: #E8CD26 !important;
+	backdrop-filter: blur(60px);
+	background: hsla(0,0%,100%,.5) !important;
+	backdrop-filter: blur(60px);
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.pages {
+	scroll-snap-type: y mandatory;
+	overflow-y: scroll;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.pages *{
+	scroll-snap-align: start;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.page {
+	height: 100vh;
+	width: 100vw;
+	background-color: rgb(22, 192, 156);
 }
 
-nav a:first-of-type {
-  border: 0;
+.p1 {
+	background-color: rgb(233, 99, 108);
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.p2 {
+	background-color: rgb(45, 83, 180);
 }
+
+.p3 {
+	background-color: rgb(22, 192, 156);
+}
+
 </style>
