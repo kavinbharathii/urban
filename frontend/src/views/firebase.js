f0d69ec94eab0251a034f1d3b3efeeff681776d1
barathbalasubramanian <barathkumar.b2411@gmail.com>
2023-03-16 16:11:52 +0530

    service cards

---
+++ frontend/src/views/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore ,collection , doc , setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD-l8J-UOWQPd3TxCqHBCtuMvFWrpR-jfY",
+  authDomain: "urban-6d98d.firebaseapp.com",
+  projectId: "urban-6d98d",
+  storageBucket: "urban-6d98d.appspot.com",
+  messagingSenderId: "580479111215",
+  appId: "1:580479111215:web:ecc34221a74888d79e76da",
+  measurementId: "G-NR66JH0WGV"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const db = getFirestore(app)
