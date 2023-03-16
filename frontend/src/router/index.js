6757bfb9b5135e18289bda7ceb0b49cd8f585b18
Kavin Bharathi <r.m.kavinbharathi@gmail.com>
2023-03-16 16:56:22 +0530

    category Services page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,19 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/HomeView.vue'
 import Computer from '../views/services/computer.vue'
+import categoryServices from '../views/services/categoryServices.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '',
-      component: Home
-    },
-    {
-      path: '/computer-services',
-      component: Computer
-    }
-  ]
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: '',
+			component: Home
+		},
+		{
+			path: '/cctv-services',
+			component: categoryServices,
+			props: {
+				categoryName: 'CCTV'
+			}
+		},
+		{
+			path: '/computer-services',
+			component: categoryServices,
+			props: {
+				categoryName: 'Computer'
+			}
+		}
+	]
 })
 
 export default router
---
+++ frontend/src/views/services/categoryServices.vue
@@ -0,0 +1,133 @@
+<script>
+
+import { db } from '../firebase.js'
+import { collection, getDocs } from "firebase/firestore";
+
+export default {
+    name : "categoryServices",
+    props: {
+        categoryName: String 
+    },
+    data() {
+        return {
+            serviceName: [],
+            providedServices: []
+        }
+    },
+    mounted: function () {
+        this.getData()
+    },
+    methods: {
+        async getData() {
+
+            const querySnapshot = await getDocs(collection(db, this.categoryName));
+            querySnapshot.forEach((doc) => {
+                this.serviceName.push(doc.id)
+                this.providedServices.push(Object(doc.data()))
+            });
+        },
+    }
+}
+</script>
+
+<template>
+    <div class="main">
+        <div class="name-img">
+            <div class="name">
+                <div class="ser-name"> {{ this.categoryName }} Services </div>
+                <div> <strong>✩</strong> 4.86 (456K Booking)</div>
+            </div>
+            <div>
+                <img src="./images/computer.png" alt="com">
+            </div>
+        </div>
+        <div class="lin"></div>
+
+        <h3>Provided Services with Pricing</h3>
+        <div class="all-cards">
+
+            <div class="cards" v-for="(data, id) in this.providedServices" :key="id">
+                <div class="detail">
+                    <div>{{ this.serviceName[id] }}</div>
+                    <div style="margin-bottom: 1em"><strong>✩</strong> {{ data.rating }} (456K Booking)</div>
+                    <div class="rupee">
+                        <strong>₹ {{ data.rupee }}.00</strong>
+                        <span> • {{ data.timing }}</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="free"></div>
+        </div>
+
+    </div>
+</template>
+
+<style scoped>
+* {
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, "Helvetica Neue", Oxygen-Sans, Ubuntu, Cantarell, Helvetica, Arial, sans-serif !important
+}
+
+.name-img {
+    padding: 5em 15%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.name .ser-name {
+    font-size: 36px;
+    font-weight: 700;
+    line-height: 48px;
+}
+
+
+.lin {
+    width: 65%;
+    background-color: #d4d4d4;
+    height: 0.3em;
+    margin: 0 15%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 4em;
+}
+
+h3 {
+    padding: 0 15%;
+    margin-bottom: 2em;
+}
+
+.all-cards {
+    padding: 0 15%;
+    display: flex;
+    flex-direction: column;
+    row-gap: 2em;
+}
+
+.rupee {
+    display: flex;
+    gap: 1em;
+    margin-bottom: 1em;
+}
+
+.view-btn {
+    background: none;
+    outline: none;
+    border: none;
+    padding: 0;
+    color: #fc3171bf;
+}
+
+.cards {
+    padding: 1em;
+    background-color: #f5f2f2;
+    ;
+}
+
+.free {
+    width: 100%;
+    height: 2em;
+}
+</style>
