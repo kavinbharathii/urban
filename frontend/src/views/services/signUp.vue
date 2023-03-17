e85f46c346d0b43b32b08a6dbc8affc8c0ff71a5
Kavin Bharathi <r.m.kavinbharathi@gmail.com>
2023-03-17 14:46:25 +0530

    signIn page - kavin

--- frontend/src/views/services/signUp.vue
+++ frontend/src/views/services/signUp.vue
@@ -5,33 +5,123 @@ import { auth } from '../firebase.js'
 
 export default {
     data() {
-        return{
-            form : { email: '' , password: '' },
+        return {
+            form: {
+                email: '',
+                password: '',
+                confirmpassword: ''
+            },
         }
     },
-    methods : {
+    methods: {
         signup() {
-            createUserWithEmailAndPassword(auth, this.form.email, this.form.password)
-                .then((userCredential) => {
-                    const user = userCredential.user;
-                    console.log('login successfully')
-                })
-                .catch((error) => {
-                    console.log(error)
-                });
+
+            if (this.form.password == this.form.confirmpassword) {
+                createUserWithEmailAndPassword(auth, this.form.email, this.form.password)
+                    .then((userCredential) => {
+                        const user = userCredential.user;
+                        console.log('login successfully')
+                    })
+                    .catch((error) => {
+                        console.log(error)
+                    });
+            } else {
+                console.error("Passwords don't match")
+            }
+
         }
     }
-
-
 }
 </script>
 
 <template>
-    <div>
-        <form @submit.prevent="signup()">
-            <label for="email">Email: <input type="text" v-model="form.email" required></label>
-            <label for="password">Password: <input type="password" v-model="form.password" required></label>
-            <button type="submit"> signup </button>
-        </form>
+    <div id="dev">
+        <div class="padder">
+            <h1>IT Arena</h1>
+            <p>Your <span></span>one spot service provider</p>
+            
+        </div>
+        <div class="former">
+            <form @submit.prevent="signup()" class="signin-form">
+                <h1>Hey there!👋</h1>
+                <label for="email">Email<input type="text" v-model="form.email" required></label>
+                <label for="password">Password<input type="password" v-model="form.password" required></label>
+                <label for="confirm-password">Confirm Password<input type="password" v-model="form.confirmpassword" required></label>
+                <div class="buttons">
+                    <button type="submit" class="prominent">Create Account</button>
+                    <button type="button" class="secondary">Already an User?</button>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
+
+<style scoped>
+#dev {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    grid-template-rows: 1fr;
+
+    width: 100vw;
+    height: 100vh;
+}
+
+.padder {
+    width: 30vw;
+    height: 100vh;
+    background-color: #fc3171bf;
+}
+
+.former {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    height: 100vh;
+}
+
+.signin-form {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+
+    gap: 1em;
+    width: 25vw;
+}
+
+label {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    gap: 10px;
+
+    width: 100%;
+}
+
+.buttons {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.buttons > button {
+    width: 45%;
+    outline: none;
+    height: 2.5em;
+    border-radius: 5px;
+    border: 1px solid #fc3171bf;
+}
+
+.prominent {
+    color: #fefefe;
+    background-color: #fc3171bf;
+}
+
+.secondary {
+    color: #171717;
+    background-color: #fefefe;
+    border: 1px solid #fc3171bf;
+}
+</style>
