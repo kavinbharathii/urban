21d8a5bb50becf6b1b65802d5ec320ca0f11240a
Kavin Bharathi <r.m.kavinbharathi@gmail.com>
2023-03-28 16:32:48 +0530

    Ui admin addservices

--- frontend/src/components/adminpage/addcategory.vue
+++ frontend/src/components/adminpage/addcategory.vue
@@ -11,7 +11,9 @@ export default {
                 collectionName: "",
                 serviceName: "",
                 price: "",
-                timing: ""
+                timing: "",
+                successState: false,
+                errorState: false
             },
         }
     },
@@ -25,37 +27,151 @@ export default {
             const newData = { rupee: this.addNewCategory.price, timing: this.addNewCategory.timing };
             const newDocRef = doc(db, collectionName, newDocID);
 
+            this.successState = false
+            this.errorState = false
+
             await setDoc(newDocRef, newData)
-            .then(() => {
-                console.log('added successfully')
-                for (let i in this.addNewCategory) {
-                    this.addNewCategory[i] = ''
-                }
-            })
-            .catch((err) => {
-                console.log('Error :',err)
-            })
-        },
+                .then(() => {
+                    console.log('added successfully')
+                    this.successState = true
+                    this.errorState = false
+                    for (let i in this.addNewCategory) {
+                        this.addNewCategory[i] = ''
+                    }
+                })
+                .catch((err) => {
+                    console.log('Error :', err)
+                    this.errorState = true
+                    this.successState = false
+                })
+        }
     }
 }
 </script>
 
 <template>
-    <div>
-        <!-- add new category -->
-        <div>
-            <form @submit.prevent="createNewCollection()">
-                <input type="text" placeholder="categoryname" v-model="addNewCategory.collectionName">
-                <input type="text" placeholder="servicename" v-model="addNewCategory.serviceName">
-                <input type="text" placeholder="timing" v-model="addNewCategory.timing">
-                <input type="number" placeholder="rupee" v-model="addNewCategory.price">
-                <input type="submit">
-            </form>
-        </div>
+    <div id="dev">
+        <div class="circle"></div>
+        <form @submit.prevent="createNewCollection()" id="categoryForm">
+            <h1>Add New Category</h1>
+            <input type="text" placeholder="categoryname" v-model="addNewCategory.collectionName">
+            <input type="text" placeholder="servicename" v-model="addNewCategory.serviceName">
+            <input type="text" placeholder="timing" v-model="addNewCategory.timing">
+            <input type="number" placeholder="rupee" v-model="addNewCategory.price">
+            <input type="submit" class="submit-button">
+
+            <div v-if="this.successState" class="info success">
+                Category added successfully
+            </div>
+    
+            <div v-if="this.errorState" class="info error">
+                Failed to add Category
+            </div>
+
+        </form>
+
     </div>
 </template>
 
 
 <style scoped>
+#dev {
+    width: 100vw;
+    height: 100vh;
 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    position: relative;
+}
+
+.circle {
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    width: 50vw;
+    height: 50vw;
+    border-radius: 0 0 50vw 0;
+
+    background-color: var(--rose-light);
+    z-index: -1;
+}
+
+#categoryForm {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    gap: 1em;
+    width: 30vw;
+
+    margin: 1em;
+    padding: 1em;
+    background-color: #fefefe;
+    border-radius: 10px;
+}
+
+#categoryForm>input {
+    width: 100%;
+    height: 2.8em;
+
+    outline: none;
+    border: 1px solid #c6c6c6;
+
+    color: #171717;
+    border-radius: 5px;
+    padding: 0 1em 0 1em;
+}
+
+#categoryForm>.submit-button {
+    background-color: var(--rose);
+
+    transition: border 200ms ease;
+}
+
+#categoryForm > .submit-button:hover {
+    border: 3px solid var(--rose-dark);
+}
+
+.info {
+    width: 100%;
+    height: 2.8em;
+    border-radius: 10px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #fefefe;
+}
+
+.success {
+    color: #22c55e;
+    border: 2px solid #22c55e;
+}
+
+.error {
+    color: #fa5f54;
+    border: 2px solid #fa5f54;
+}
+
+@media only screen and (max-width: 1000px) {
+    #categoryForm {
+        width: 50vw;
+    }
+
+    .circle {
+        width: 80vw;
+        height: 80vw;
+        border-radius: 0 0 100vh 0;
+    }
+}
+
+@media only screen and (max-width: 600px) {
+    #categoryForm {
+        width: 100vw;
+    }
+}
 </style>
--- frontend/src/components/adminpage/addservices.vue
+++ frontend/src/components/adminpage/addservices.vue
@@ -3,7 +3,7 @@
 
 import { db, db_rt } from "@/views/firebase.js"
 import { get, ref } from "@firebase/database";
-import { setDoc, doc, getDocs, collection } from "@firebase/firestore";
+import { setDoc, doc, getDocs, getFirestore } from "firebase/firestore";
 
 export default {
     data() {
@@ -18,9 +18,10 @@ export default {
                 "Computer",
                 "CCTV"
             ],
+            collections: []
         }
     },
-    methods : {
+    methods: {
 
         // add a new service
         addNewService() {
@@ -34,7 +35,7 @@ export default {
                     rupee: this.newServiceForm.price,
                     timing: this.newServiceForm.timing
                 }).then(() => {
-                    
+
                     for (let i in this.newServiceForm) {
                         this.newServiceForm[i] = ''
                     }
@@ -45,35 +46,34 @@ export default {
                 })
             }
         },
+    },
+
+    async mounted() {
+        const snapshot = await db.listCollections();
+        snapshot.forEach(doc => {
+            if (doc.path.split('/').length === 2) {
+                this.collections.push(doc.path.split('/')[1]);
+            }
+        });
     }
 }
 </script>
 
 <template>
-    <div>
-        <form id="add-new-service">
-            <h3>Add new service</h3>
-            <label for="">
-                Service Name:
-                <input type="text" v-model="this.newServiceForm.serviceName">
-            </label>
+    <div id="dev">
+        <div class="circle"></div>
+        <form id="serviceForm">
+            <h1>Add new service</h1>
+            <input type="text" placeholder="service name" v-model="this.newServiceForm.serviceName">
 
-            <label for="">
-                Category:
-                <select required v-model="this.newServiceForm.categoryName" >
-                    <option :value="{category}" v-for="(category , index) in this.categories" :key="index">{{ category }}</option>
-                </select>
-            </label>
+            <select required placeholder="select category" v-model="this.newServiceForm.categoryName">
+                <option :value="{ category }" v-for="(category, index) in this.categories" :key="index">{{ category }}
+                </option>
+            </select>
 
-            <label for="">
-                Price:
-                <input type="number" v-model="this.newServiceForm.price">
-            </label>
+            <input type="text" placeholder="timing" v-model="this.newServiceForm.timing">
 
-            <label for="">
-                Timing:
-                <input type="text" v-model="this.newServiceForm.timing">
-            </label>
+            <input type="number" placeholder="price" v-model="this.newServiceForm.price">
 
             <button @click.prevent="addNewService">Add Service</button>
         </form>
@@ -82,10 +82,104 @@ export default {
 
 
 <style scoped>
-#add-new-service {
+#dev {
+    width: 100vw;
+    height: 100vh;
+
     display: flex;
-    flex-direction: column;
     justify-content: center;
     align-items: center;
+
+    position: relative;
+}
+
+.circle {
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    width: 50vw;
+    height: 50vw;
+    border-radius: 0 0 50vw 0;
+
+    background-color: var(--rose-light);
+    z-index: -1;
+}
+
+#serviceForm {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    gap: 1em;
+    width: 30vw;
+
+    margin: 1em;
+    padding: 1em;
+    background-color: #fefefe;
+    border-radius: 10px;
+}
+
+#serviceForm>input,
+select {
+    width: 100%;
+    height: 2.8em;
+
+    outline: none;
+    border: 1px solid #c6c6c6;
+
+    color: #171717;
+    border-radius: 5px;
+    padding: 0 1em 0 1em;
+}
+
+#serviceForm>.submit-button {
+    background-color: var(--rose);
+
+    transition: border 200ms ease;
+}
+
+#serviceForm>.submit-button:hover {
+    border: 3px solid var(--rose-dark);
+}
+
+.info {
+    width: 100%;
+    height: 2.8em;
+    border-radius: 10px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #fefefe;
+}
+
+.success {
+    color: #22c55e;
+    border: 2px solid #22c55e;
+}
+
+.error {
+    color: #fa5f54;
+    border: 2px solid #fa5f54;
+}
+
+@media only screen and (max-width: 1000px) {
+    #serviceForm {
+        width: 50vw;
+    }
+
+    .circle {
+        width: 80vw;
+        height: 80vw;
+        border-radius: 0 0 100vh 0;
+    }
+}
+
+@media only screen and (max-width: 600px) {
+    #serviceForm {
+        width: 100vw;
+    }
 }
 </style>
